34d08f866bb0d0c6cb3afd5c880d010f392f3727
parkjiu0208 <joy002208@gmail.com>
2024-07-19 18:47:29 +0900

    feat: first commit

---
+++ feelinsight/src/main/java/com/fellinsight/FellinsightApplication.java
@@ -0,0 +1,13 @@
+package com.fellinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FellinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FellinsightApplication.class, args);
+	}
+
+}
