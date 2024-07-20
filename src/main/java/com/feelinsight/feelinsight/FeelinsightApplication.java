61f953c5335e723d9e905b48002c6036e174dedb
parkjiu0208 <joy002208@gmail.com>
2024-07-21 03:24:36 +0900

    test commit

---
+++ src/main/java/com/feelinsight/feelinsight/FeelinsightApplication.java
@@ -0,0 +1,13 @@
+package com.feelinsight.feelinsight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeelinsightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeelinsightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/feelinsight/feelinsight/FeelinsightApplicationTests.java
@@ -0,0 +1,13 @@
+package com.feelinsight.feelinsight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeelinsightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
